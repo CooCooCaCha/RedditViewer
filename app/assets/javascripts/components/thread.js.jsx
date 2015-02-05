ecbacca6ad770a4d9f9d854acce21bcb0879d169
William Johnson <williamjohnson@MacAndMe.local>
2015-02-04 21:04:53 -0500

    Added thumnails to thread list.

--- app/assets/javascripts/components/thread.js.jsx
+++ app/assets/javascripts/components/thread.js.jsx
@@ -4,6 +4,7 @@ var Thread = React.createClass({
     author: React.PropTypes.string,
     link: React.PropTypes.string,
     score: React.PropTypes.node,
+    thumbnail: React.PropTypes.string,
 
     onClick: React.PropTypes.func
   },
@@ -21,11 +22,26 @@ var Thread = React.createClass({
         marginBottom: '10px',
         margin: '5px',
         backgroundColor: '#fff',
-        padding: '5px'
+        padding: '5px',
+        overflow: 'hidden'
     };
+
+    var thumbStyle = {
+        float: 'left',
+        width: '64px',
+        height: '64px',
+        marginRight: '10px'
+    };
+
+    var thumbnail = null;
+    if( this.props.thumbnail !== 'self' && this.props.thumbnail !== 'default' )
+        thumbnail = (
+            <img src={this.props.thumbnail} style={thumbStyle} />
+        );
 
     return (
       <div style={threadStyle}>
+        {thumbnail}
         <div style={titleStyle} onClick={this.props.onClick.bind( null, this.props.link )}>
             <span dangerouslySetInnerHTML={{__html: this.props.title}} />
         </div>
--- app/assets/javascripts/components/threadlist.js.jsx
+++ app/assets/javascripts/components/threadlist.js.jsx
@@ -34,7 +34,7 @@ var ThreadList = React.createClass({
   render: function() {
     var threadNodes = this.state.threads.map( function( thread ) {
         return (
-            <Thread title={thread.title} author={thread.author} score={thread.score} link={thread.id} onClick={this.props.onThread} />
+            <Thread title={thread.title} author={thread.author} score={thread.score} link={thread.id} thumbnail={thread.thumbnail} onClick={this.props.onThread} />
         );
     }.bind(this));
 
